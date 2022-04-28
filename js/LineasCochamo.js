bc4836aae0c1e841a9f087872eaf4f9300368383
José Miguel <48037080+jmrojas1@users.noreply.github.com>
2022-04-28 14:51:12 -0400

    Update LineasCochamo.js

--- js/LineasCochamo.js
+++ js/LineasCochamo.js
@@ -9,19 +9,34 @@ function addLines(data) {
 		"features": []
 	}
 
-	var liness = {
-		"color": "#f2f3f7",
+	var LinesGeologico = {
+		"color": "#ff0000",
 		"dashArray": "10",
 		"weight": 4,
-		"opacity": 0.4,
+		"opacity": 0.9,
 		"pane": "LinesPane"
 	}
 
-	var linesecologico = {
-		"color": "#f06102",
+	var LinesEcologico = {
+		"color": "#79e83d",
 		"dashArray": "10",
 		"weight": 4,
-		"opacity": 0.4,
+		"opacity": 0.9,
+		"pane": "LinesPane"
+	}
+	
+	var LinesBiologico = {
+		"color": "#f3bb3b",
+		"dashArray": "10",
+		"weight": 4,
+		"opacity": 0.9,
+		"pane": "LinesPane"
+	}
+
+	var LinesHidrologico = {
+		"color": "#0084a8",
+		"weight": 4,
+		"opacity": 0.9,
 		"pane": "LinesPane"
 	}
 
@@ -39,6 +54,7 @@ function addLines(data) {
 			"Descripcion": data.descripcion,
 			"Significacion": data.significacion,
 			"Referencias": data.referencias,
+			"Registro": data.registro,
 			"Foto": data.foto,
 			"Hipervinculo": data.hipervinculo
 		}
@@ -48,8 +64,10 @@ function addLines(data) {
 
 		style: function (Feature) {
 			switch (Feature.properties.Subcategoria) {
-				case "Natural": return liness;
-				case "Ecológico": return linesecologico;
+				case "Geológico": return LinesGeologico;
+				case "Ecológico": return LinesEcologico;
+				case "Biológico": return LinesBiologico;
+				case "Hidrológico": return LinesHidrologico;
 			}
 		},
 
@@ -60,9 +78,15 @@ function addLines(data) {
 			function openSidebar(e) {
 				sidebar.show();
 				{
-					sidebar.setContent("<h3>" + "<a href=" + Feature.properties.Hipervinculo + " target=_blank>" + Feature.properties.Elemento + "</a></h3>" + "<img src = " + Feature.properties.Foto + " width=100%>" + "<p>" + Feature.properties.Descripcion + "</p>" + "<ul>" + "<li><b>Ubicaci&oacute;n:&nbsp;</b>" + Feature.properties.Ubicacion + "</li>" + "<li><b>Significaci&oacute;n cultural:&nbsp;</b>" + Feature.properties.Significacion + "</li>" + "<li><b>Referencias Bibliogr&aacute;ficas:&nbsp;</b>" + Feature.properties.Referencias + "</li>" + "</ul>")
+					sidebar.setContent("<h3>" + "<a href=" + Feature.properties.Hipervinculo + " target=_blank>" + Feature.properties.Elemento + "</a></h3>" + "<img src = " + Feature.properties.Foto + " width=100%>" + "<p>" + Feature.properties.Descripcion + "</p>" + "<ul>" + "<li><b>Ubicaci&oacute;n:&nbsp;</b>" + Feature.properties.Ubicacion + "</li>" + "<li><b>Significaci&oacute;n cultural:&nbsp;</b>" + Feature.properties.Significacion + "</li>" + "<li><b>Referencias Bibliogr&aacute;ficas:&nbsp;</b>" + Feature.properties.Referencias + "<li><b>Registro:&nbsp;</b>" + Feature.properties.Registro + "</li>" + "</ul>")
 				}
 			};
+		switch (Feature.properties.Subcategoria) {
+			case "Geológico": return GeologicoGroup.addLayer(layer);
+			case "Ecológico": return EcologicoGroup.addLayer(layer);
+			case "Biológico": return BiologicoGroup.addLayer(layer);
+			case "Hidrológico": return HidrologicoGroup.addLayer(layer);
+			}
 		}
 	});
 }
