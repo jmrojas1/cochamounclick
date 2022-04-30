2e17b5653dfc9c80442ddb9a4c46cac00b25e52c
José Miguel <48037080+jmrojas1@users.noreply.github.com>
2022-04-30 09:22:38 -0400

    Update LineasCochamo.js

--- js/LineasCochamo.js
+++ js/LineasCochamo.js
@@ -73,8 +73,9 @@ function addLines(data) {
 
 		onEachFeature: function (Feature, layer) {
 			layer.bindPopup("<b>" + Feature.properties.Elemento + "</b>");
+			layer.bindTooltip("<b>" + Feature.properties.Elemento + "</b>", {sticky: true});
 			layer.on({ click: openSidebar });
-
+			
 			function openSidebar(e) {
 				sidebar.show();
 				{
